a65f31bb64fad9c0bf9296189bda0365b9d0857a
MrLu <46912289+PinkamenaDianeLu@users.noreply.github.com>
2021-05-26 14:48:36 +0800

    添加java对websocket调用

--- JZZL/src/main/java/com/module/Interface/Controllers/WebSocketClientUtil.java
+++ JZZL/src/main/java/com/module/Interface/Controllers/WebSocketClientUtil.java
@@ -4,30 +4,76 @@ package com.module.Interface.Controllers;/**
  * @describe
  */
 
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
 /**
  * @author MrLu
  * @createTime 2021/5/24 9:48
  * @describe
  */
-public class WebSocketClientUtil {
-
-    public  WebSocketClientUtil  getClient(String url){
-       /* try {
-            WebSocketClient client=new WebSocketClient(new URI(url)){
-
-                @Override
-                public ListenableFuture<WebSocketSession> doHandshake(WebSocketHandler webSocketHandler, String uriTemplate, Object... uriVariables) {
-                    return null;
-                }
-
-                @Override
-                public ListenableFuture<WebSocketSession> doHandshake(WebSocketHandler webSocketHandler, WebSocketHttpHeaders headers, URI uri) {
-                    return null;
-                }
-            };
-        }catch (Exception e){
-
-        }*/
-        return  null;
+public class WebSocketClientUtil extends WebSocketClient{
+    public WebSocketClientUtil(URI serverUri, Draft draft) {
+        super(serverUri, draft);
     }
+
+    public WebSocketClientUtil(URI serverURI) {
+        super(serverURI);
+    }
+
+    public WebSocketClientUtil(URI serverUri, Map<String, String> httpHeaders) {
+        super(serverUri, httpHeaders);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake serverHandshake) {
+        send("测试发送websocket连接");
+        System.out.println("发送连接文本");
+    }
+
+     /**
+     * 接收到消息
+     * @author MrLu
+     * @param 
+     * @createTime  2021/5/26 14:45
+     * @return    |  
+      */
+    @Override
+    public void onMessage(String s) {
+        System.out.println("received: " + s);
+    }
+
+     /**
+     * 断开连接
+     * @author MrLu
+     * @param 
+     * @createTime  2021/5/26 14:47
+     * @return    |  
+      */
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("Connection closed by " + (remote ? "remote peer" : "us") + " Code: " + code + " Reason: " + reason);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        e.printStackTrace();
+    }
+
+    public static void main(String[] args) throws URISyntaxException, InterruptedException {
+        WebSocketClientUtil c = new WebSocketClientUtil(new URI("ws://localhost:9003")); // more about drafts here: http://github.com/TooTallNate/Java-WebSocket/wiki/Drafts
+        c.connectBlocking();
+
+//        c.getReadyState();
+        c.send("客户端第二次发送");
+        c.send("1");
+//        c.close(1,"我关了啊");
+
+    }
+
 }
